a18f0ebc0c6f34dca2ebc75465c7d9e2601bb3cd
Doğukan <dogu_kutluay_1999@hotmail.com>
2022-01-30 01:41:08 +0300

    next

---
+++ .next/static/mDREV3y8RsiNydxH8mtg3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/120-de13e6f9b8ec8c2f.js","static/css/3b31b1a3d144e66d.css","static/chunks/pages/index-b777bdf8c79e376e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
