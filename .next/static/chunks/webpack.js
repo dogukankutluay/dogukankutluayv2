c846b4f2fee8bad212f8c88b55fdf33fbc06aaa2
dogukan <dogu_kutluay_1999@hotmail.com>
2023-03-22 17:09:50 +0300

    deleted meta tag

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9e018621afce9af3"; }
+/******/ 		__webpack_require__.h = function() { return "a4d1f77f3943a6fb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
